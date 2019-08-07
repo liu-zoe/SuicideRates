7ffc72365432383129bbbe24875a4e545e2eb913
Zoe Liu <zoelzh@gmail.com>
2019-08-06 21:01:04 -0500

    Adding data from Gallup World Poll (merging yet to be done)

--- SR-Data.py
+++ SR-Data.py
@@ -6,15 +6,16 @@
 #%%[markdown]
 # Import packages for data processing
 import csv
-import pandas as pd 
-
-#%% load in suicide data
+import pandas as pd
+import numpy as np
+import xlrd
+#%% [markdown]
+#### load in suicide data
 # master data contains the suicide rates
 master=pd.read_csv('C:/Users/liuz2/Documents/Projects/SuicideRates/master.csv')
 #%%[markdown]
 #Get a list of the variable names
-vars=list(master)
-
+vars_master=list(master)
 #%% [markdown]
 #### Unemployment data
 ##### The reason of choosing National Estimates instead of OIL estimates
@@ -29,6 +30,7 @@ uem_totl=uem[uem['Indicator Code']=='SL.UEM.TOTL.NE.ZS']
 uem_ma=uem[uem['Indicator Code']=='SL.UEM.TOTL.MA.NE.ZS']
 #Female unemployment (% of total labor force) National Estimates
 uem_fe=uem[uem['Indicator Code']=='SL.UEM.TOTL.FE.NE.ZS']
+
 #%% Create a dataset with country code and country name
 country=uem[['Country Name', 'Country Code']]
 country.drop_duplicates(keep='first',inplace=True)
@@ -57,11 +59,43 @@ del uem_fe, uem_ma, uem_totl
 master=pd.merge(master, country, how='left', on=['country'])
 master=pd.merge(master, uem, how='left', left_on=['iso3c','year'], right_on=['iso3c','year'])
 
-
-##Book Mark:--> The following block is not yet completed.
 #%% Create a variable UEM that is gender specific using sex, UEM_MA, and UEM_FE
-#master=master.assign(UEM=lambda x: x['UEM_MA'] if x['sex']=='male' else x['UEM_FE'])
+master['UEM']=np.where(master['sex']=='male', master['UEM_MA'], master['UEM_FE'])
 
+#%%
+# whr19 data contains the world happiness data published in 2019 
+whr19=pd.read_csv('C:/Users/liuz2/Documents/Projects/SuicideRates/WHR19.csv')
+#%%[markdown]
+#### Gallup World Poll
+##### Access to Internet
+# Do you have access to the Internet in any way, whether on a mobile phone, a computer, or some other device?
+gallup=pd.read_excel('C:/Users/liuz2/Documents/Projects/SuicideRates/Gallup_20190806.xlsx', \
+    sheet_name='Access to the Internet', skiprows=7, usecols='B:C, E:F')
+#%%
+# Pivot the data
+gallup.columns=['country','year','gender','value']
+gallup=gallup.pivot_table(values='value', index=['country','year'],columns='gender')
+internet=gallup.reset_index()
+internet.columns=['country','year','internet','internet_f','internet_m']
+del gallup
+#%%[markdown] 
+##### Happiness (Life Ladder)
+# Please imagine a ladder with steps numbered from 0 at the bottom to 10 at the top. 
+# Suppose we say that the top of the ladder represents the best possible life for you, 
+# and the bottom of the ladder represents the worst possible life for you. 
+# On which step of the ladder would you say you personally feel you stand at this time, 
+# assuming that the higher the step the better you feel about your life, 
+# and the lower the step the worse you feel about it? Which step comes closest to the way you feel?
+
+# Load in data on Happiness
+gallup=pd.read_excel('C:/Users/liuz2/Documents/Projects/SuicideRates/Gallup_20190806.xlsx', \
+    sheet_name='Life Today', skiprows=7, usecols='B:C, E:F')
+# Pivot the data by gender
+gallup.columns=['country','year','gender','value']
+gallup=gallup.pivot_table(values='value', index=['country','year'],columns='gender')
+happy=gallup.reset_index()
+happy.columns=['country','year','happy','happy_f','happy_m']
+del gallup
 
 #%%[markdown] 
 #### Take a look at the key variabless
