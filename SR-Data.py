950ca2eec2d65040beb5f091fc52ae28442551af
Liu <Zoe.Liu@bsci.com>
2019-08-09 11:58:29 -0500

    Merging WHR19, Gallup data into the master dataset

--- SR-Data.py
+++ SR-Data.py
@@ -8,7 +8,7 @@
 import csv
 import pandas as pd
 import numpy as np
-import xlrd
+#import xlrd
 #%% [markdown]
 #### load in suicide data
 # master data contains the suicide rates
@@ -62,22 +62,62 @@ master=pd.merge(master, uem, how='left', left_on=['iso3c','year'], right_on=['is
 #%% Create a variable UEM that is gender specific using sex, UEM_MA, and UEM_FE
 master['UEM']=np.where(master['sex']=='male', master['UEM_MA'], master['UEM_FE'])
 
-#%%
+#%% [markdown]
+# Create a dataset with country code and region name
+cdat=pd.read_csv("C:/Users/liuz2/Documents/Projects/World Bank/whatawaste/country_level_data_2.csv", encoding="utf-8")
+region=cdat[['region_id','country_name','income_id']]
+# Mapping Region Labels against Region ID
+regions= {
+    'ECS': 'Europe and Central Asia',
+    'SSF': 'Sub-Saharan Africa',
+    'LCN': 'Latin America and the Caribbean',
+    'EAS': 'East Asia and Pacific',
+    'MEA': 'Middle East and North Africa',
+    'SAS': 'South Asia',
+    'NAC': 'North America', 
+}
+region['region']=region['region_id'].map(regions)
+# Mapping Income Labels against Income ID
+incomes={
+    'HIC': 'High-income',
+    'UMC': 'Upper-middle income',
+    'LMC': 'Lower-middle income',
+    'LIC': 'Low-income'
+}
+region['income']=region['income_id'].map(incomes)
+region.columns=['region_id','country','income_id','region','income']
+master=pd.merge(master, region, how='left',left_on=['country'], right_on=['country'])
+del cdat
+
+#%% [markdown]
 # whr19 data contains the world happiness data published in 2019 
 whr19=pd.read_csv('C:/Users/liuz2/Documents/Projects/SuicideRates/WHR19.csv')
+whr19.columns=['country','year','LifeLadder','LogGDPPC','Social_support','Healthy_life_expectancy',\
+    'Freedom_to_make_life_choice','Generosity','Perceptions_of_corruption','positive_affect','negative_affect',\
+        'Confidence_in_government','Democratic_quality','Delivery_quality',\
+        'LifeLadder_std', 'LifeLadder_std_mean', 'gini', 'gini_avg', 'gini_house',\
+        'trust', 'trust84', 'trust93', 'trust98', 'trust04', 'trust09', 'trust14']
+master=pd.merge(master, whr19, how='left', left_on=['country','year'], right_on=['country','year'])
+
 #%%[markdown]
 #### Gallup World Poll
 ##### Access to Internet
 # Do you have access to the Internet in any way, whether on a mobile phone, a computer, or some other device?
 gallup=pd.read_excel('C:/Users/liuz2/Documents/Projects/SuicideRates/Gallup_20190806.xlsx', \
     sheet_name='Access to the Internet', skiprows=7, usecols='B:C, E:F')
-#%%
 # Pivot the data
 gallup.columns=['country','year','gender','value']
 gallup=gallup.pivot_table(values='value', index=['country','year'],columns='gender')
 internet=gallup.reset_index()
 internet.columns=['country','year','internet','internet_f','internet_m']
 del gallup
+# print(min(internet['year']), max(internet['year']))
+# Unfortunately, the access to internet data from the Gallup World Poll 
+# ranges from 2015 to 2018, which only overlap two years with the suicide 
+# rate data, which ranges from 1985 to 2016.
+
+# Merge the internet data (2015 and 2016) to the SR data
+master=pd.merge(master, internet, how='left', left_on=['country','year'], right_on=['country','year'])
 #%%[markdown] 
 ##### Happiness (Life Ladder)
 # Please imagine a ladder with steps numbered from 0 at the bottom to 10 at the top. 
@@ -97,22 +137,28 @@ happy=gallup.reset_index()
 happy.columns=['country','year','happy','happy_f','happy_m']
 del gallup
 
+# Merge the happiness data (2006 to 2016) to the SR data
+master=pd.merge(master, happy, how='left', left_on=['country','year'], right_on=['country','year'])
+
 #%%[markdown] 
 #### Take a look at the key variabless
-#Country
+# Country
 master['country'].value_counts()
 #%%[markdown]
-# Year
-master['year'].value_counts()
+# Region
+master['region'].value_counts().sort_index()
 #%%[markdown]
-#Gender Groups
-master['sex'].value_counts()
+# Year
+master['year'].value_counts().sort_index()
+#%%[markdown]
+# Gender Groups
+master['sex'].value_counts().sort_index()
 #%%[markdown] 
 # Age Groups
-master['age'].value_counts()
+master['age'].value_counts().sort_index()
 #%%[markdown] 
 # Generation Groups
-master['generation'].value_counts()
+master['generation'].value_counts().sort_index()
 #%%[markdown] 
 # Suicide Counts
 master['suicides_no'].describe()
